c7e80c7d65921af1817041027e752c2b681ced12
Peter Wilson <wilson@peterwilson.cc>
2010-10-27 12:41:06 +1100

    add search form

---
+++ searchform.php
@@ -0,0 +1,12 @@
+<?php
+//make sure id isn't specified twice on the same page
+//no need to seed since php 4.2.0
+$soup_safeIds = rand(1, 999999);
+?>
+<form method="get" action="<?php bloginfo('home'); ?>" class="search-form">
+<div>
+	<label for="s-<?php echo $soup_safeIds; ?>" class="search-label">Search this Site</label>
+	<input type="text" name="s" id="s-<?php echo $soup_safeIds; ?>" class="searchInput inputText" />
+	<input type="submit" value="Search" class="searchSubmit inputSubmit" />
+</div>
+</form>
