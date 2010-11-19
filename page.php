814ee2f3a8d0b4b0071acf04a01711628796c4b7
Peter Wilson <wilson@peterwilson.cc>
2010-11-19 15:01:58 +1100

    remove links from single and page headings ( #pageName )

--- page.php
+++ page.php
@@ -8,7 +8,7 @@ the_post();
 	<article id="contentHeadA" <?php post_class(); ?>>
 		<header id="contentHead">
 			<<?php echo $soup->pageNameTag; ?> id="pageName" class="entry-title">
-				<a href="<?php the_permalink();?>"><?php the_title()?></a>
+				<?php the_title()?>
 			</<?php echo $soup->pageNameTag; ?>>
 		</header>			
 		
--- single.php
+++ single.php
@@ -8,7 +8,7 @@ the_post();
 	<article id="contentHeadA" <?php post_class(); ?>>
 		<header id="contentHead">
 			<<?php echo $soup->pageNameTag; ?> id="pageName" class="entry-title">
-				<a href="<?php the_permalink();?>"><?php the_title()?></a>
+				<?php the_title()?>
 			</<?php echo $soup->pageNameTag; ?>>
 			<p>Posted on <time datetime="<?php the_time('c') ?>" pubdate class="entry-date"><?php the_time(get_option('date_format')); ?></time> by <span class="author vcard"><a class="url fn n" href="<?php echo get_author_posts_url( $authordata->ID, $authordata->user_nicename ); ?>" title="View all posts by <?php the_author(); ?>"><?php the_author(); ?></a></span></p>
 		</header>			
