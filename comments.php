acda66efb8268e03f4696a7b93896d1cfc1cf55b
Peter Wilson <wilson@peterwilson.cc>
2010-11-23 17:34:41 +1100

    change comment form to use div's rather than p's for input pairs
    add inputPair class to each of these

--- comments.php
+++ comments.php
@@ -76,23 +76,27 @@ if ( 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']) )
 	<section>
 		<?php comment_form(array(
 			'fields' => apply_filters('comment_form_default_fields', array(
-				'author' => '<p class="comment-form-author">' . '<label for="author">' . __( 'Name' ) . '</label> ' 
+				'author' => '<div class="comment-form-author inputPair">' . '<label for="author">' . __( 'Name' ) . '</label> ' 
 							. ( $req ? '<span class="required">*</span>' : '' ) .
 				            '<input id="author" name="author" type="text" value="' . 
 							esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req .
-							( $req ? ' required' : '' ) . ' /></p>',
-				'email'  => '<p class="comment-form-email"><label for="email">' . __( 'Email' ) . '</label> ' .
+							( $req ? ' required' : '' ) . ' /></div>',
+				'email'  => '<div class="comment-form-email inputPair"><label for="email">' . __( 'Email' ) . '</label> ' .
 							( $req ? '<span class="required">*</span>' : '' ) .
 				            '<input id="email" name="email" type="email" value="' . 
 							esc_attr(  $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . 
-							( $req ? ' required' : '' ) . ' /></p>',
-							'url'    => '<p class="comment-form-url"><label for="url">' . __( 'Website' ) . '</label>' .
-							            '<input id="url" name="url" type="url" value="' . 
-										esc_attr( $commenter['comment_author_url'] ) . '" size="30" /></p>'
+							( $req ? ' required' : '' ) . ' /></div>',
+				'url'    => '<div class="comment-form-url inputPair"><label for="url">' . __( 'Website' ) . '</label>' .
+				            '<input id="url" name="url" type="url" value="' . 
+							esc_attr( $commenter['comment_author_url'] ) . '" size="30" /></div>'
 				)),
 				'comment_notes_after'  => '<p class="form-allowed-tags">' . 
 						sprintf( __( 'You may use these <abbr>HTML</abbr> tags and attributes: %s' ), ' <code>' .
-						allowed_tags() . '</code>' ) . '</p>'
+						allowed_tags() . '</code>' ) . '</p>',
+				'comment_field' => '<div class="comment-form-comment inputPair"><label for="comment">' . 
+							_x( 'Comment', 'noun' ) . '</label>' . 
+							'<textarea id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea></div>',
+		
 			)); ?>
 			
 			
